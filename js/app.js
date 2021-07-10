a4b6d60fecfbce97f4d1a88db95977c3d7499f3c
Jordi Jiménez Coves <jjimenezcoves@gmail.com>
2021-07-10 13:30:35 +0200

    some examples

--- js/app.js
+++ js/app.js
@@ -3,9 +3,30 @@
 var app = angular.module('universidadApp',[ ]);
 
 app.controller('listadoCtrl', ['$scope', function($scope){
-	
-		
 
+    $scope.listado = ["Fernando Herrera", "Melissa Flores", "Juan Carlos Pineda", "Maria Perez"];
+
+    $scope.listadoProfesores = {
+        profesores: [
+            {
+                nombre: "Fernando Herrera",
+                edad: 29,
+                clase: "PEE"
+            },
+            
+            {
+                nombre: "Melissa Flores",
+                edad: 24,
+                clase: "ICE"
+            },
+            {
+                nombre: "Juan Carlos Pineda",
+                edad: 42,
+                clase: "M110"
+            }
+
+        ]
+    };
 
 
 }]);
